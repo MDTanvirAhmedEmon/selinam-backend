d8f3ec427b2d26115c612fb1d911211a3e6da9ba
Tanvir Ahmed Emon <132162447+MDTanvirAhmedEmon@users.noreply.github.com>
2025-07-05 07:53:50 +0600

    Update transaction.controller.ts

--- src/app/modules/transaction/transaction.controller.ts
+++ src/app/modules/transaction/transaction.controller.ts
@@ -1,24 +1,45 @@
-import { NextFunction, Request, Response } from "express";
-import { transactionServices } from "./transaction.services";
+import { NextFunction, Request, Response } from 'express'
+import { transactionServices } from './transaction.services'
 
-const createTransaction = async (req: Request, res: Response, next: NextFunction) => {
-    try {
+const createTransaction = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const data = req.body
+    const result = await transactionServices.createTransaction(data)
 
-        const data = req.body
-        const result = await transactionServices.createTransaction(data)
-
-        res.status(200).json({
-            success: true,
-            message: 'transaction created successfully',
-            result,
-        })
-    }
-    catch (error) {
-        next(error)
-    }
+    res.status(200).json({
+      success: true,
+      message: 'transaction created successfully',
+      result,
+    })
+  } catch (error) {
+    next(error)
+  }
 }
 
+const getSpecificRecentCost = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const query = req.query
+    const result = await transactionServices.getSpecificRecentCost(query)
+
+    res.status(200).json({
+      success: true,
+      message: 'get specific recent transaction successfully',
+      result,
+    })
+  } catch (error) {
+    next(error)
+  }
+}
 
 export const transactionController = {
-    createTransaction,
+  createTransaction,
+  getSpecificRecentCost,
 }
