2881a5f09e6088e8a5ab2fa4b86eb4491d6798c6
Tanvir Ahmed Emon <132162447+MDTanvirAhmedEmon@users.noreply.github.com>
2025-07-05 07:53:33 +0600

    Update transaction.route.ts

--- src/app/modules/transaction/transaction.route.ts
+++ src/app/modules/transaction/transaction.route.ts
@@ -1,12 +1,12 @@
-import { Router } from "express";
-import { transactionController } from "./transaction.controller";
+import { Router } from 'express'
+import { transactionController } from './transaction.controller'
 
-const router = Router();
+const router = Router()
 
 router.post('/create', transactionController.createTransaction)
 
-// router.get('/', categoryController.getAllCategory)
+router.get('/recent-cost', transactionController.getSpecificRecentCost)
 // router.delete('/:id', categoryController.deleteCategory)
 // router.patch('/:id', upload.single('file'), categoryController.updateCategory)
 
-export const TransactionRouter = router;
+export const TransactionRouter = router
